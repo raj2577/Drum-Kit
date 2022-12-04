be7f000eb5056eb484b7f61287c1817afa1656ed
raj2577 <112565295+raj2577@users.noreply.github.com>
2022-12-04 22:43:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+document.querySelectorAll("button")[0].addEventListener("click", handleClick);
+
+function handleClick() {
+    
+    var buttonInnerHTML =this.innerHTML
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play();
+            
+            break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play();
+            
+            break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play();
+            
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play();
+            
+            break;
+        case "j":
+            var crash = new Audio('sounds/kick-bass.mp3')
+            crash.play();
+            
+            break;
+        case "k":
+            var kick = new Audio('sounds/crash.mp3')
+            kick.play();
+            
+            break;
+        case "l":
+            var snare = new Audio('sounds/snare.mp3')
+            snare.play();
+            
+            break;
+    
+        default:console.log("problem")
+            break;
+    }
+    makeSound(buttonInnerHTML)
+   
+}
+var but = 0;
+var numOfButton = 7
+
+// with while loop
+
+// while (but < numOfButton ){
+//     document.querySelectorAll(".drum")[but].addEventListener("click", handleClick);
+//     but++ ;
+// }
+
+//same loop with for loop 
+
+for (var i = 0; i<numOfButton ; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+document.addEventListener("keydown", function(event){
+    console.log(event);
+    makeSound(event.key)
+})
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play();
+            
+            break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play();
+            
+            break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play();
+            
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play();
+            
+            break;
+        case "j":
+            var crash = new Audio('sounds/kick-bass.mp3')
+            crash.play();
+            
+            break;
+        case "k":
+            var kick = new Audio('sounds/crash.mp3')
+            kick.play();
+            
+            break;
+        case "l":
+            var snare = new Audio('sounds/snare.mp3')
+            snare.play();
+            
+            break;
+    
+        default:console.log("problem")
+            break;
+    }
+   
+}
